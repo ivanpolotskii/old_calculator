21468f21696f25ff0b4e0525bab247b6ce281a40
mentalist01 <58345789+mentalist01@users.noreply.github.com>
2020-06-15 18:44:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+let c = document.querySelector('.c'),
+    delet=document.querySelector('.delete'),
+    parentheses = document.querySelector('.parentheses'),
+    percent = document.querySelector('.percent'),
+    splitUp = document.querySelector('.split-up'),
+    multiply = document.querySelector('.multiply'),
+    minus = document.querySelector('.minus'),
+    plus = document.querySelector('.plus'),
+    num = document.querySelectorAll('.num button'),
+    comma = document.querySelector('.comma'),
+    equally = document.querySelector('.equally'),
+    rez = document.querySelector('#rez');
+
+parentheses.addEventListener('click', () => {
+    if (rez.value.lastIndexOf('(') > rez.value.lastIndexOf(')')) {
+        rez.value += ')';
+    } else {
+        if (rez.value[rez.value.length - 1] == '+' || rez.value[rez.value.length - 1] == '/' || rez.value[rez.value.length - 1] == '*' || rez.value[rez.value.length - 1] == '-') {
+            rez.value += '(';
+        }
+    }
+});
+percent.addEventListener('click', () => {
+    rez.value = rez.value + '%';
+});
+num.forEach(num => {
+    num.addEventListener('click', (e) => {
+        rez.value += num.textContent;
+    });
+});
+plus.addEventListener('click', function () {
+    if (rez.value[rez.value.length - 1] !== '+' && rez.value[rez.value.length - 1] !== '/' 
+        && rez.value[rez.value.length - 1] !== '*' &&
+        rez.value[rez.value.length - 1] !== '-' && rez.value !== '') {
+        rez.value += '+';
+    }
+
+});
+multiply.addEventListener('click', () => {
+    if (rez.value[rez.value.length - 1] !== '+' && rez.value[rez.value.length - 1] !== '/' 
+        && rez.value[rez.value.length - 1] !== '*' &&
+        rez.value[rez.value.length - 1] !== '-' && rez.value !== '') {
+            rez.value += '*';
+    }
+    
+});
+minus.addEventListener('click', () => {
+    if (rez.value[rez.value.length - 1] !== '+' && rez.value[rez.value.length - 1] !== '/' 
+        && rez.value[rez.value.length - 1] !== '*' &&
+        rez.value[rez.value.length - 1] !== '-' && rez.value !== '') {
+            rez.value += '-';
+    }
+    
+});
+delet.addEventListener('click',()=>{
+    rez.value = rez.value.slice(0,rez.value.length-1);
+});
+c.addEventListener('click', () => {
+    rez.value = '';
+});
+equally.addEventListener('click', () => {
+    rez.value = eval(rez.value);
+});
+comma.addEventListener('click', () => {
+    rez.value += '.';
+});
